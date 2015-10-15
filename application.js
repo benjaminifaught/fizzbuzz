d5f5dfc437ffab2a13a77fb71756b9d22500debd
benjaminifaught <benjaminifaught>
2015-10-15 17:44:58 -0400

    show in html

--- application.js
+++ application.js
@@ -1,11 +1,25 @@
-
+$(document).ready(function() {
+var s = "";
 for (var i = 1; i < 101; i++) {
-	if (i % 15 == 0)
+	if (i % 15 == 0) {
 		console.log("FizzBuzz");
-	else if (i % 5 == 0) 
+		s = "FizzBuzz"; 
+		$(".list").append(s+ "<br>"); 
+	}
+	else if (i % 5 == 0)  {
 		console.log("Fizz");
-	else if (i % 3 == 0)
+		s = "Fizz";
+		$(".list").append(s+ "<br>"); 
+	}
+	else if (i % 3 == 0) {
 		console.log("Buzz");
-   	else 
+		s = "Buzz";
+		$(".list").append(s+ "<br>"); 
+	}
+   	else {
    		console.log(i);
+   		s = i;
+   		$(".list").append(s+ "<br>"); 
+   	}
 } 
+});
